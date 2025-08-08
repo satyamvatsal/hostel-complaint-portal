cbf004c7523bdddfdea5b171edbd55c6c04a8703
satyam <satyamvatsal257@gmail.com>
2025-08-08 22:18:25 +0530

    added backend

--- db/db.js
+++ db/db.js
@@ -1,2 +1,14 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/test");
+require("dotenv").config({ path: "./.env.local" });
+
+const MONGO_URL = process.env.MONGO_URI;
+
+const connectDB = async () => {
+  const db = await mongoose
+    .connect(MONGO_URL)
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.error("Failed to connect to mongodb, ", err));
+  console.log(db);
+};
+
+module.exports = connectDB;
--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+const connectDB = require("./db/db");
 const express = require("express");
 const app = express();
+require("dotenv").config({ path: ".env.local" });
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
+app.use(express.static(path.join(__dirname, "public")));
+
+connectDB();
+
 app.get("/register", (req, res) => {
   res.render("register");
 });
 
-const port = 1080;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
