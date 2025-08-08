3c9d6a6f240f9624619040bbd72fbae4d3c4b1a5
satyam <satyamvatsal257@gmail.com>
2025-08-08 21:40:28 +0530

    added views

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.get("/register", (req, res) => {
+  res.render("register");
 });
 
 const port = 1080;
