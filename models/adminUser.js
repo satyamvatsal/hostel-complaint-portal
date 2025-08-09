3e90b8bc9a243d0200aeb88792d72953d7db7671
satyam <satyamvatsal257@gmail.com>
2025-08-09 23:42:32 +0530

    added admin model

---
+++ models/adminUser.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const adminUserSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    hostel_no: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.Model("AdminUser", adminUserSchema);
