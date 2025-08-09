6d74962041d018e7bf48d8cc125202dcee646682
satyam <satyamvatsal257@gmail.com>
2025-08-09 14:37:50 +0530

    updated routers

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 const userRouter = require("./routes/userRouter");
+const complainRouter = require("./routes/complainRouter");
 require("dotenv").config({ path: ".env.local" });
 
 app.set("view engine", "ejs");
@@ -14,6 +15,7 @@ app.use(express.static("./public"));
 connectDB();
 
 app.use("/user", userRouter);
+app.use("/complain", complainRouter);
 
 const port = process.env.PORT;
 app.listen(port, "0.0.0.0", () => {
