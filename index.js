dbcde0d6e7ae9a449f09c76c662878edeeae07bc
satyam <satyamvatsal257@gmail.com>
2025-08-09 15:11:49 +0530

    updated routes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const userRouter = require("./routes/userRouter");
 const complainRouter = require("./routes/complainRouter");
+const authMiddleare = require("./middleware/auth");
 require("dotenv").config({ path: ".env.local" });
 
 app.set("view engine", "ejs");
@@ -15,7 +16,8 @@ app.use(express.static("./public"));
 connectDB();
 
 app.use("/user", userRouter);
-app.use("/complain", complainRouter);
+app.use("/complaint", authMiddleare, complainRouter);
+app.get("/", (req, res) => res.redirect("/user"));
 
 const port = process.env.PORT;
 app.listen(port, "0.0.0.0", () => {
